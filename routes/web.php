1e4cfcd5af9d49abb934513828c571da69f93794
LaupWing <loc_strict@hotmail.com>
2020-05-07 16:24:33 +0200

    i think it works now

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/pizzas', function () {
+    $pizzas = [
+        ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+        ['type' => 'volcano', 'base' => 'garlic crust'],
+        ['type' => 'veg supreme', 'base' => 'thin & crispy']
+    ];
+    return view('pizzas', ['pizzas' => $pizzas]);
+});
